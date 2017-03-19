7f73f0c49bdc688fea584cb37ff6ada63da2fca4
JB515 <jpb515@gmail.com>
2017-03-19 16:59:51 +0000

    Structure and a bit of text for the report

--- pid.cpp
+++ pid.cpp
@@ -1,8 +1,25 @@
 //All Speeds are rotations per second
-double pid(double targetVelocity, double prevVelocity, double &sumError){
+double pidVelocity(double targetVelocity, double prevVelocity, double &sumError){
 	double error = targetVelocity - currentVelocity;
 	sumError += error;
 	double prevError = targetVelocity - prevVelocity;
+
+	double kp = 10;
+	double ki = 0;
+	double kd = 0;
+
+
+	double proportional = kp * error;
+	double intergral = ki * sumError;
+	double differential = kd * (error - prevError);
+
+	return proportional + integral + differential;
+}
+
+double pidPosition(double targetPosition, double prevPosition, double &sumError){
+	double error = targetPosition - currentPosition;
+	sumError += error;
+	double prevError = targetPosition - prevPosition;
 
 	double kp = 10;
 	double ki = 0;
